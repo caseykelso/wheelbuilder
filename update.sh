bfe57a9b6d75458d2e48d274b3ac7d36a583b791
Casey Kelso <casey@caseykelso.com>
2014-06-22 19:30:01 -0600

    + update

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH=${ANDROID_HOME}/tools
 expect -c '
 set timeout -1;
-spawn /Users/ckelso/android-sdk-macosx/tools/android update sdk --filter platform-tools,android-19,extra-android-support --no-ui;
+spawn /Users/ckelso/android-sdk-macosx/tools/android update sdk --filter platform-tools,android-19,extra-android-support --no-ui --force;
 expect {
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
